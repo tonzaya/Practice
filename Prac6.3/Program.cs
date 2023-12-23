7b1b163d376a88ccab617135a07d7659c671420e
tonzaya <154662994+tonzaya@users.noreply.github.com>
2023-12-23 16:11:43 +0700

    Add files via upload

---
+++ Prac6.3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+        
+bool isEvenAndMultipleOfTen = false;
+        
+if (number % 2 == 0 && number % 10 == 0) {
+    isEvenAndMultipleOfTen = true;
+}
+        
+Console.WriteLine($"Число {number} {(isEvenAndMultipleOfTen ? "четное и кратно 10" : "не является четным и/или кратным 10")}");
+
